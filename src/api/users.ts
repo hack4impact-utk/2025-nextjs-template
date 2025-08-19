5588ba6a3d666a81d3e3472eab6ee456fbb19472
Rudra Patel <patelrudra2003@gmail.com>
2025-08-19 10:24:56 -0400

    feat: Create example CRUD functions

---
+++ src/api/users.ts
@@ -0,0 +1,35 @@
+import db from "@/db";
+import { User } from "@/types/schema";
+
+export async function getAllUsers(): Promise<User[]> {
+  return await db.query.users.findMany();
+}
+
+/**
+ *
+ * These three functions are examples of how to use the db object to perform CRUD operations.
+ * The create, update, and delete functions are server actions (as indicated by the "use server" directive
+ * and can be called from the client side.
+ * In the future, make sure these functions are protected by authentication.
+ */
+
+// export async function createUser(user: User): Promise<User> {
+//   "use server";
+//   const newUser = await db.insert(users).values(user).returning();
+//   return newUser[0];
+// }
+
+// export async function updateUser(user: User): Promise<User> {
+//   "use server";
+//   const updatedUser = await db
+//     .update(users)
+//     .set(user)
+//     .where(eq(users.id, user.id))
+//     .returning();
+//   return updatedUser[0];
+// }
+
+// export async function deleteUser(id: string): Promise<void> {
+//   "use server";
+//   await db.delete(users).where(eq(users.id, id));
+// }
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,9 @@
+import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-const db = drizzle(process.env.DATABASE_URL);
+import * as schema from "./schema";
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle({ client: sql, schema });
 
 export default db;
